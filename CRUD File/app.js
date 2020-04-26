484680be252a971dea313e24bdd947d4efb5f265
mohitgupta004 <guptamohi.009@gmail.com>
2020-04-26 12:52:09 +0530

    crud file update

--- CRUD File/app.js
+++ CRUD File/app.js
@@ -3,28 +3,48 @@ var app = express();
 var fs = require('fs');
 
 app.get('/read', (req, res) => {
-    fs.readFile('./data.json', 'utf8', (err, jsonString) => {
+    fs.readFile('./user.json', 'utf8', (err, jsonString) => {
         if (err) {
             console.log("File read failed:", err);
             return;
         }
-        res.send(jsonString)
+        res.send({
+            'msg':'File Data',
+            jsonString
+        })
     });
 })
 
-app.get('/add', (req, res) => {
+app.post('/add', (req, res) => {
     const customer = {
-        "name": "mohit",
-        "email": "abc@gmail.com",
-        "phone": 123,
-        "city": "mohit"
+        "name": req.body.name,
+        "email": req.body.email,
+        "phone": req.body.phone,
+        "city": req.body.city
     };
-    fs.readFile('./data.json', function(err, data) {
+    fs.readFile('./user.json', function(err, data) {
         var json = JSON.parse(data);
         json.push(customer);
-        fs.writeFile("./data.json", JSON.stringify(json), function(err) {
+        fs.writeFile("./user.json", JSON.stringify(json), function(err) {
             if (err) throw err;
             res.send('The "data to append" was appended to file!');
+        });
+    });
+})
+
+app.post('/delete',(req,res)=>{
+    fs.readFile('./user.json', function(err, data) {
+        var json = JSON.parse(data);
+        console.log("Data before Deletion");
+        console.log(json)
+        json=json.filter(data=>{
+            return data.name!=(req.body.name)
+        })
+        console.log("Data After Deletion");
+        console.log(json)
+        fs.writeFile("./user.json", JSON.stringify(json), function(err) {
+            if (err) throw err;
+            res.send('The data was deleted from Users');
         });
     });
 })
